9c5fc59d08a60052ecc3b653317b26cf31d6b0e7
hajuli99 <hajuli99@9869a2f8-016d-11de-b507-8b4ecd5210be>
2009-04-12 09:27:01 +0000

    第一次创建

---
+++ coding/talkboard/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls.defaults import *
+
+import settings
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+import os
+urlpatterns = patterns('',
+    # Example:
+    # (r'^talkboard/', include('talkboard.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/(.*)', admin.site.root),
+    (r'^site-media/(.*)$', 'django.views.static.serve', {'document_root': 'media'}),
+
+    #(r'^log/', include('web.log.URLconf')),
+    (r'^$', 'home.views.index'),
+)
