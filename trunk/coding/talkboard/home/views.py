3a593b3e78bd470e26d687a4ea640378a90f7fe5
hajuli99 <hajuli99@9869a2f8-016d-11de-b507-8b4ecd5210be>
2009-04-15 15:01:51 +0000

    可以添加删除收藏了。

--- trunk/coding/talkboard/home/views.py
+++ trunk/coding/talkboard/home/views.py
@@ -1,8 +1,16 @@
 # Create your views here.
 # coding=utf-8
 from django.shortcuts import render_to_response
-from favorites import models
+from favorites import models as models_fv
 
 def index(request):
-    fvs = models.get_all_favorites()
-    return render_to_response("home.html", {"fvs": fvs})
+    fvs = models_fv.get_all_favorites()
+    datas = []
+    i = 0
+    for xx in fvs:
+        e = {}
+        e["index"] = unicode(i % 6)
+        e["value"] = xx
+        datas.append(e)
+        i += 1
+    return render_to_response("home.html", {"datas": datas})
