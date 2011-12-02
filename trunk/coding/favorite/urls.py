4d13ae2d492e363f64d3e3c39b0a06f70558de6a
hajuli99@gmail.com <hajuli99@gmail.com@9869a2f8-016d-11de-b507-8b4ecd5210be>
2011-12-02 17:45:11 +0000

    change name from talkboard to favorite

---
+++ trunk/coding/favorite/urls.py
@@ -0,0 +1,28 @@
+from django.conf.urls.defaults import *
+
+import settings
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+import os
+urlpatterns = patterns('',
+    # Example:
+    # (r'^talkboard/', include('talkboard.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/(.*)', admin.site.root),
+    (r'^favorite/site-media/(.*)$', 'django.views.static.serve', {'document_root': 'media'}),
+
+    #(r'^log/', include('web.log.URLconf')),
+    (r'^favorite/favorites/add', 'favorites.views.index'),
+    (r'^favorite/favorites/save', 'favorites.views.save_favorites'),
+    (r'^favorite/favorites/delete', 'favorites.views.delete_favorites'),
+    (r'^favorite/userpanel/', include('userpanel.URLconf')),
+    (r'^favorite/$', 'home.views.index'),
+    (r'^$', 'home.views.index'),
+)
