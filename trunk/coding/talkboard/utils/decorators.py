bc922b8c8f0064ecb5478024bdee2e8e369ca491
hajuli99@gmail.com <hajuli99@gmail.com@9869a2f8-016d-11de-b507-8b4ecd5210be>
2011-12-02 17:46:40 +0000

    change name from talkboard to favorite

--- trunk/coding/talkboard/utils/decorators.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.decorators import user_passes_test
-superuser_required = user_passes_test(lambda u: u.is_superuser)
-superuser_required.__doc__ = (
-    """
-    Decorator for views that checks that the user is superuser, redirecting
-    to the log-in page if necessary.
-    """
-    )
